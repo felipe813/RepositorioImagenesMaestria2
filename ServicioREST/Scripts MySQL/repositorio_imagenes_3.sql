a18f3941b57b4709c0708bd8507801b9437208d5
felipe813 <felipe_813@hotmail.com>
2022-03-27 22:46:58 -0500

    Ajsute script

--- ServicioREST/Scripts MySQL/repositorio_imagenes_3.sql
+++ ServicioREST/Scripts MySQL/repositorio_imagenes_3.sql
@@ -1,4 +1,4 @@
 USE repositorionuevo ;
-UPDATE preguntatest SET Pregunta = '¿Que tan amigable le pareció la pantalla de carga en cuanta a mensajes, diseño y progreso?' WHERE IdPregunta = 1;
-UPDATE preguntatest SET Pregunta = '¿Qué tan cómodo lo pareció esperar el inicio del recorrido?' WHERE IdPregunta = 4;
-UPDATE preguntatest SET Pregunta = '¿Qué tan realista le pareció la interacción con el “álbum de fotos”?' WHERE IdPregunta = 5;
+UPDATE PreguntaTest SET Pregunta = '¿Que tan amigable le pareció la pantalla de carga en cuanta a mensajes, diseño y progreso?' WHERE IdPregunta = 1;
+UPDATE PreguntaTest SET Pregunta = '¿Qué tan cómodo lo pareció esperar el inicio del recorrido?' WHERE IdPregunta = 4;
+UPDATE PreguntaTest SET Pregunta = '¿Qué tan realista le pareció la interacción con el “álbum de fotos”?' WHERE IdPregunta = 5;
